9c95f2d4039709ce51bc8ecc53dc7c6edbeca5ef
Jesse Harlin <harlinjesse@gmail.com>
2015-04-02 01:53:16 -0500

    testing infrastructure and gulp tasking added

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+var plato = require('gulp-plato');
+var runSequence = require('run-sequence');
+var coveralls = require('gulp-coveralls');
+var taskListing = require('gulp-task-listing');
+
+function test(cb) {
+
+  var mochaOpts = {
+    reporter: 'nyan'
+  };
+
+  function runner() {
+    gulp
+      .src(['./test/main.js'])
+      .pipe(mocha(mochaOpts))
+      .pipe(istanbul.writeReports())
+
+    .on('end', cb);
+  }
+
+  gulp
+    .src(['./index.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', runner);
+}
+
+
+function complexity() {
+
+  var jsHintArgs = {
+      options: {
+        strict: true
+      }
+    },
+    complexityArgs = {
+      trycatch: true
+    },
+    platoArgs = {
+      jshint: jsHintArgs,
+      complexity: complexityArgs
+    };
+
+
+  gulp.src(['slushfile.js'])
+    .pipe(plato('plato', platoArgs));
+}
+
+function lcov() {
+  gulp
+    .src('coverage/**/lcov.info')
+    .pipe(coveralls());
+}
+
+
+function ci(cb) {
+  runSequence('test', 'complexity', cb);
+}
+
+gulp
+  .task('default', taskListing)
+  .task('test', test)
+  .task('complexity', complexity)
+  .task('coveralls', lcov)
+  .task('ci', ci);
---
+++ test/default.js
@@ -0,0 +1,20 @@
+'use strict';
+var chai = require('chai'),
+  inquirer = require('inquirer'),
+  gulp = require('gulp'),
+  mockGulpDest = require('mock-gulp-dest')(gulp),
+  expect = chai.expect();
+
+require('../slushfile');
+
+describe('slush-phaser-webpack', function () {
+  before(function () {
+    process.chdir(__dirname);
+  });
+  
+  describe('default', function(){
+    
+  
+  });
+
+});
