560c32b47382f2a28b41728e8134f2077092fa2a
Jesse Harlin <harlinjesse@gmail.com>
2015-04-08 23:14:03 -0500

    added some default application

---
+++ templates/game.js
@@ -0,0 +1,5 @@
+var Phaser = require('phaser').Phaser;
+
+game = new Phaser.Game(600, 600, Phaser.AUTO, 'phaser_game');
+
+module.exports = game;
---
+++ templates/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+var game = require('./game');
+var states = require('./states');
+
+
+
+
+
+
+
+game.state.start('boot');
