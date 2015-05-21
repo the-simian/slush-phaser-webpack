7dd3b587e8de10b23630e8a66a516b94da759d2e
the-simian <harlinjesse@gmail.com>
2015-05-20 23:39:13 -0500

    improved boot state

--- templates/default/src/game.js
+++ templates/default/src/game.js
@@ -2,6 +2,6 @@ global.PIXI = require('pixi.js');
 global.p2 = require('p2');
 global.Phaser = require('phaser')
 
-game = new Phaser.Game(600, 600, Phaser.AUTO, 'phaser_game');
+game = new Phaser.Game(800, 800, Phaser.AUTO, 'phaser_game');
 
 module.exports = game;
