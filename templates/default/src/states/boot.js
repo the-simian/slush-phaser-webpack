cf28fb8a5e76095568b66fe8802e81e2c15ba72d
the-simian <harlinjesse@gmail.com>
2015-05-20 23:35:44 -0500

    improved boot state slightly

--- templates/default/src/states/boot.js
+++ templates/default/src/states/boot.js
@@ -1,12 +1,13 @@
 var game = require('../game'),
-    Phaser = require('phaser').Phaser;
+  Phaser = require('phaser').Phaser;
 
+var fragmentSrc = require('../shaders/example.frag');
 
-var fragmentSrc = require('../shaders/example.frag'),
-  filter,
+var filter,
   sprite;
 
 function createBootState() {
+
   var headerText = 'Boot State';
 
   var headerTextStyle = {
@@ -15,33 +16,24 @@ function createBootState() {
     align: 'center'
   };
 
-
   filter = new Phaser.Filter(game, null, fragmentSrc);
-  filter.setResolution(800, 600);
+  filter.setResolution(800, 800);
 
   sprite = game.add.sprite();
   sprite.width = 800;
-  sprite.height = 600;
-
+  sprite.height = 800;
   sprite.filters = [filter];
 
-
   var headText = game.add.text(game.world.centerX, 32, headerText, headerTextStyle);
-
-
 }
-
 
 function updateBootState() {
-
   filter.update(game.input.activePointer);
 }
-
 
 var boot = {
   create: createBootState,
   update: updateBootState
 };
-
 
 module.exports = boot;
